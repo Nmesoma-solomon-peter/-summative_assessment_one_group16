7a0d2cf8008636e63864e04cca226492bbfe8c28
Q-Reine <q.wihogora@alustudent.com>
2024-06-18 15:19:41 +0200

    Validation

--- app.js
+++ app.js
@@ -13,31 +13,41 @@
 // linking form elements to js variables
 const nameInput = document.querySelector("#fullName");
 const emailInput = document.querySelector("#email");
-// const phoneInput = document.querySelector("#phone");
-// const linkedinInput = document.querySelector("#linkedin");
+const phoneInput = document.querySelector("#phone");
+const linkedinInput = document.querySelector("#linkedin");
 // const coverLetterInput = document.querySelector("#coverLetter");
 // const SalaryInput = document.querySelector("#salary");
 const submitBtn = document.querySelector("#submit");
 
-// regex patterns 
+// regex patterns
 const fullNameRegex = /^[A-Za-z]+ [A-Za-z\s]+$/
 const EmailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 const phoneRegex = /^(\d{3})-\d{3}-\d{4}$/
-const linkedinRegex = /^https:\/\/www\.linkedin\.com\/[a-zA-Z0-9_-]+$/
+// const linkedinRegex = /^https:\/\/www\.linkedin\.com\/in\/[a-zA-Z0-9_-]+$/
+const linkedinRegex = /^https:\/\/www\.linkedin\.com\/in\/[a-zA-Z0-9-]+\//
 const coverLetterRegex = /^[a-zA-Z0-9\s.,!?]*$/
 const expectedSalarayRegex = /^\d+(\.\d{1,2})?$/
 
+https://www.linkedin.com/in/nmesoma-peter-5785b9209/
 
 submitBtn.addEventListener("click",()=>{
     let nameInputValue = nameInput.value;
     if(fullNameRegex.test(nameInputValue)){
         if(EmailRegex.test(emailInput.value)){
+            if(phoneRegex.test(phoneInput.value)){
+                if(linkedinRegex.test(linkedinInput.value)){
 
+                }else{
+                    alert("invalid Url");
+                }
+            }else{
+                alert("Invalid Phone Number format");
+            }
         }else{
             alert("wrong email format");
         }
     }else{
         alert("name must be in pairs and must not contain special characters or numbers")
-    }
+    }
 
 })
